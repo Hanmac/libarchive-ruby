2496e78e6981b73f6b118b33d9ba727492786ca8
Hanmac <hanmac@gmx.de>
2011-03-05 14:01:32 +0100

    fixed main.hpp

--- ext/main.hpp
+++ ext/main.hpp
@@ -22,14 +22,12 @@ with libarchive-ruby; if not, write to the Free Software Foundation, Inc.,
 
 #ifndef __RubyAchiveMain_H__
 #define __RubyAchiveMain_H__
-
 #include <ruby.h>
 #include <archive.h>
 #include <archive_entry.h>
 #include <fcntl.h>
 #include <string>
 extern VALUE rb_cArchive,rb_cArchiveEntry;
-
 void Init_archive_entry(VALUE m);
 
 template <typename T>
@@ -39,10 +37,16 @@ VALUE wrap(T arg){ return Qnil;};
 template <typename T>
 T wrap(const VALUE &arg){};
 
-struct rarchive{
-	std::string path;
-};
+enum archive_type {archive_path,archive_fd,archive_buffer};
 
+struct rarchive{
+ //union drumrum?
+	std::string path;
+	
+	std::string buffer;
+	int fd;
+	archive_type type;
+};
 
 template <>
 inline VALUE wrap< rarchive >(rarchive *file )
@@ -60,22 +64,6 @@ inline rarchive* wrap< rarchive* >(const VALUE &vfile)
   Data_Get_Struct( vfile, rarchive, file);
 	return file;
 }
-/*
-inline time_t VALUEtime(VALUE t){
-	time_t result = time(NULL);
-	t = rb_funcall(t,rb_intern("getutc"),0);
-	tm *timeinfo = gmtime(&result);
-	timeinfo->tm_year = NUM2INT(rb_funcall(t,rb_intern("year"),0)) - 1900;
-	timeinfo->tm_mday = NUM2INT(rb_funcall(t,rb_intern("mday"),0));
-	timeinfo->tm_yday = NUM2INT(rb_funcall(t,rb_intern("yday"),0)) - 1;
-	timeinfo->tm_mon = NUM2INT(rb_funcall(t,rb_intern("mon"),0)) - 1;
-	timeinfo->tm_hour = NUM2INT(rb_funcall(t,rb_intern("hour"),0));
-	timeinfo->tm_min = NUM2INT(rb_funcall(t,rb_intern("min"),0));
-	timeinfo->tm_sec = NUM2INT(rb_funcall(t,rb_intern("sec"),0));
-	timeinfo->tm_isdst =rb_funcall(t,rb_intern("isdst"),0) == Qtrue ? 1 : 0;
-	return mktime (timeinfo);
-}
-*/
 template <>
 inline VALUE wrap< archive_entry >(struct archive_entry *entry )
 {
